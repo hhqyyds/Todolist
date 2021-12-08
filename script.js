c212d21b79d179ac67e0df0dd5cd6574e4b32c0e
hhqyyds <2417602086@qq.com>
2021-12-08 23:06:22 +0800

    提交二：待办事项列表排版样式

--- script.js
+++ script.js
@@ -16,8 +16,37 @@ function renderTodoItemList(todoItems){
 
    for(let item of todoItems){
        let itemDiv =document.createElement("div");
-       itemDiv.innerText=item.title;
+       itemDiv.className="todo-item" 
+       let inputEl =document.createElement("input");
+       inputEl.type ="checkbox" 
+
+       let titleEl =document.createElement("div");
+       titleEl.className="title"
+       let importanceEl =document.createElement("div");
+       importanceEl.className="important"
+       importanceEl.innerText="!"
+       let deleteEl =document.createElement("div");
+       deleteEl.className="delete"
+       deleteEl.innerText="x"
+
+
+       titleEl.innerText=item.title;
+
+       itemDiv.append(inputEl);
+       itemDiv.append(titleEl);
+       itemDiv.append(importanceEl);
+       itemDiv.append(deleteEl) 
        paneEl.append(itemDiv);
    }
 }
+
+function renderInputPane(todoItems){
+    let todoListEl=document.getElementById("todolist");
+    let addBtnEl=todoListEl.querySelector("#add-btn");
+    addBtnEl.addEventListener("click",(e)=>{
+        console.log("add a item")
+    });
+    //Slet btnEl= document.querySelector("#todolist #add-btn");
+}
 renderTodoItemList(todoItems)
+renderInputPane(todoItems)
