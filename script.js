7e706f5bf1b13ab7f16a317edc9787700e712f89
hhqyyds <2417602086@qq.com>
2021-12-17 18:43:52 +0800

    提交五：新增输入框有内容才可以添加，增加标题，改善界面样式

--- script.js
+++ script.js
@@ -8,47 +8,47 @@ function renderTodoItemList(todoItems,finishedItems){
 
    for(let i=0;i<todoItems.length;i++){
        let item=todoItems[i];
-       let itemDiv =document.createElement("div");
-       itemDiv.className="todo-item" 
+       let itemDiv =document.createElement("div"); 
+       itemDiv.className="todo-item";
        
        let inputEl =document.createElement("input");
        inputEl.type ="checkbox";
-       inputEl.addEventListener("change",(e)=>{
-            finishedItems.push(item);
-            todoItems.splice(i,1);
+       inputEl.addEventListener("change",(e)=>{                         //勾选代表完成该事项，自动加入finishedItems=[]，//
+            finishedItems.push(item);                                   //todoItems=[]通过splice删除该项//
+            todoItems.splice(i,1);                            
             console.log("finished:",i,todoItems,finishedItems);
             renderTodoItemList(todoItems,finishedItems);
        })
 
-       let titleEl =document.createElement("div");
-       titleEl.className="title"
+       let contentEl =document.createElement("div");
+       contentEl.className="content"
        
        let importanceEl =document.createElement("div");
-       importanceEl.className="important"
+       importanceEl.className="important";
        importanceEl.innerText="!";
         if(item.isImportance){
         importanceEl.classList.add("open")
-       }
-        importanceEl.addEventListener("click",(e)=>{
+       }                                                               //给事项设置是否重要，在样式中设定了按钮颜色，//
+        importanceEl.addEventListener("click",(e)=>{                   //点击按钮表示事项重要，便会显示红色，再次点击则取消//
         if (item.isImportance){
             item.isImportance=false;
         }else{item.isImportance=true;
         }
         renderTodoItemList(todoItems,finishedItems);
-    })
+        })
    
-       let deleteEl =document.createElement("div");
-       deleteEl.className="delete"
+       let deleteEl =document.createElement("div");                   //新增加的删除按钮，点击会删除该事项//
+       deleteEl.className="delete"                                    //根据前面多个按钮的代码和splice的功能尝试写的//
        deleteEl.innerText="x"
        deleteEl.addEventListener("click",(e)=>{ 
         todoItems.splice(i,1);
         renderTodoItemList(todoItems,finishedItems);
        });
 
-       titleEl.innerText=item.title;
+       contentEl.innerText=item.content;
 
        itemDiv.append(inputEl);
-       itemDiv.append(titleEl);
+       itemDiv.append(contentEl);
        itemDiv.append(importanceEl);
        itemDiv.append(deleteEl) 
        paneEl.append(itemDiv);
@@ -66,8 +66,8 @@ function renderFinishedItemList(todoItems,finishedItems){
         let itemDiv =document.createElement("div");
         itemDiv.className="todo-item";
         
-        let titleEl =document.createElement("div");
-        titleEl.className="title"
+        let contentEl =document.createElement("div");
+        contentEl.className="content"
 
         let importanceEl =document.createElement("div");
         importanceEl.className="important"
@@ -76,9 +76,9 @@ function renderFinishedItemList(todoItems,finishedItems){
             importanceEl.classList.add("open")
         }
  
-        titleEl.innerText=item.title;
+        contentEl.innerText=item.content;
     
-        itemDiv.append(titleEl);
+        itemDiv.append(contentEl);
         itemDiv.append(importanceEl);
          
         paneEl.append(itemDiv);
@@ -90,21 +90,22 @@ function renderInputPane(todoItems,finishedItems){
     let hisBtnEl=inputPaneEl.querySelector("#his-btn");
     addBtnEl.addEventListener("click",(e)=>{
         let inputEl=inputPaneEl.querySelector("input");
+        if(inputEl.value!=""){
             if(todoItems.push({ 
-                title:inputEl.value,
+                content:inputEl.value,
                 isFinished:false,
                 isImportance:false,
             })){
-                inputEl.value="";
-            }
-        
-        
-    
-        console.log("add a item:",inputEl.value);
+                inputEl.value="";    //新增：输入框中若没有输入内容，则不会添加事项//
+            }                        //点击添加标志，输入框的内容会加到下面的待办清单里，//
+        }                            //并且新增加了输入框的内容自动清除功能以便再次输入//
+                                     //在样式表中还增加了换行功能，输入文字过多可自动换行//
 
-        renderTodoItemList(todoItems,finishedItems);
-        
-    });
+            console.log("add a item:",inputEl.value);
+        renderTodoItemList(todoItems,finishedItems);      //每次添加都会调用待办清单的函数，重新刷新清单//
+    });                                                   //并且方便起见令待办清单的innerHTML为空，优化显示效果//
+                                                          //todoItems=[]相当于容器，如果不让innerHTML为空，//
+                                                          //每次调用函数都会读取里面的所有内容，之前读过的内容会重复，达不到效果//
     hisBtnEl.addEventListener("click",(e)=>{
         if (hisBtnEl.classList.contains("open")){
             hisBtnEl.classList.remove("open");
@@ -114,6 +115,7 @@ function renderInputPane(todoItems,finishedItems){
             renderFinishedItemList(todoItems,finishedItems)
         }
     }); 
+                                       //历史记录按钮，可在待办清单和历史记录页面之间切换//
 
     //Slet btnEl= document.querySelector("#todolist #add-btn");
 }
